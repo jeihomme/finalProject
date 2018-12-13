a652df3f442540554948095e017e469d816865cd
jinw <nada2781@naver.com>
2018-12-13 13:06:27 +0900

    12/ 13일 board 작업
    구인구직 컨트롤단에 메소드 설정

--- finalProject/src/main/java/web/controller/AdvertiseController.java
+++ finalProject/src/main/java/web/controller/AdvertiseController.java
@@ -1,13 +1,50 @@
 package web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import web.service.face.AdvertiseService;
 
 @Controller
 public class AdvertiseController {
+	
+	private Logger logger = LoggerFactory.getLogger(AdvertiseController.class);
 
 	@Autowired AdvertiseService advertiseSerivce;
 	
+	@RequestMapping(value="/advertise/list", method=RequestMethod.GET)
+	public void list() {
+		
+		logger.info("구인구직 리스트");
+		
+	}
+	
+	public void write() {
+		logger.info("글쓰기 폼");
+		
+	}
+	
+	public String writeProc() {
+		logger.info("글쓰기 처리");
+		
+		return null;
+	}
+	
+	public void update() {
+		logger.info("글수정 폼");
+	}
+	
+	public String updateProc() {
+		logger.info("글수정 처리");
+		
+		return null;
+	}
+	
+	public void delete() {
+		
+	}
 }
